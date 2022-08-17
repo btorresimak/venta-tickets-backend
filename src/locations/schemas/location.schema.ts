24ab184e172bc11ef2661ee5b6e313c902c6400b
Bryan Torres <bryantorresckp@gmail.com>
2022-08-17 10:10:02 -0500

    Aggregar precio de las entradas

--- src/locations/schemas/location.schema.ts
+++ src/locations/schemas/location.schema.ts
@@ -5,6 +5,7 @@ export const locationSchema = new Schema(
     name: { type: String, required: true, unique: true, uppercase: true },
     total: { type: Number, required: true },
     available: { type: Number, required: true },
+    price: { type: Number },
     isActive: { type: Boolean, default: true },
   },
   {
